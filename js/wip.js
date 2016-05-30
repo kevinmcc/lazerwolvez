9104f12fdc5156e48dd7b50812b19280b2b851f9
Kevin McCutcheon <kevin.mccutcheon@gladeye.co.nz>
2016-05-30 14:38:57 +1200

    about page loop through Jquery

--- js/wip.js
+++ js/wip.js
@@ -1,20 +1,36 @@
 lazerObj = {
 	
-	'bang-bang-wolf': {
-		image:'url/path/to/image.gif',
-		audio:'url/path/to/audio-loop.mp3'
+	'url1': {
+		image:'1.gif',
+		audio:'loop/sound1.wav'
+	},
+
+	'url2': {
+		image:'2.gif',
+		audio:'loop/sound2.wav'
+	},
+
+	'url3': {
+		image:'3.gif',
+		audio:'loop/sound3.wav'
+	},
+
+	'url4': {
+		image:'4.gif',
+		audio:'loop/sound4.wav'
 	}
 	
 }
 
-
+var html = "";
 
 for(var prop in lazerObj){
 	var url = prop;
-
 	var image = lazerObj[prop].image;
 	var audio = lazerObj[prop].audio;
 
+	html += '<div class="grid-item" data-music="sound1.wav" data-time="4890" style="background-image: url(images/'+image+');"><div class="content"><h2>'+audio+'</h2><h3>Music: was this</h3></div></div>';
 
-	var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+	$('#grid-container').html(html);
+
 }
