e37f161de20cd6f8dd3c0aa2a87c0afe6e050407
James Armenta <armentajames@gmail.com>
2016-06-13 16:05:16 +1200

    Offset Grid w/ Rotate on Hover for Wolves

--- js/main.js
+++ js/main.js
@@ -1,51 +1,185 @@
 $(document).ready(function(){
-//expand wolf on index page
-$('a[href="javascript:openwolf();"').click(function(){
-	wolf = $(this).parent().find('.wolfgif');
-	alert(wolf.prop('tagName'));
 
-	offset = $(wolf).offset();
-	width = $(wolf).width();
-	height = $(wolf).height();
+	var wolves = [] ;
 
-	bg = $(wolf).css('background');
-	$('body').prepend('<a href="#"><div class="full"></div></a>');
+	var mouse = {
+		x:0,
+		y:0
+	}
 
-	$('.full').css({"background": bg});
-	$('.full').css({"width": "500px", "height": "500px"});
-	$('.full').css({"top": offset.top-$(window).scrollTop()});//scrollTop allows negative
-	$('.full').css({"left": offset.left});
-	$('.full').css({"width": width});
-	$('.full').css({"height": height});
+	var lastMouse = {
+		x:0,
+		y:0
+	}
+	var on  = false;
+	var target;
 
-	$('.full').animate({
-		left: 0,
-		top: 0,
-		width: "100vw",
-		height: "100vh",
-		backgroundSize: "15%",
-	}, 400);
-
-});
-
-num = 1;
-$('body').on('click','.full', function(){
-	num = (num%6)+1;
-	//$('.full').css({"width": "100px"})
-	$('.full').css({"background": "url('./images/"+num+".gif')"})
-});
-
-$('a[href="#home"]').click(function(){
-	target = $('.grid').find('.wolfgif');
-
-	$('.full').animate({
-		left: target.offset().left,
-		top: target.offset().top-$(window).scrollTop(),
-		width: target.width(),
-		height: target.height(),
-	}, 400,function(){
-		$('.full').remove();
+	window.addEventListener('mousemove', function(e){
+		mouse.x = e.clientX;
+		mouse.y = e.clientY;
+		lastMouse.x += (mouse.x - lastMouse.x)/10;
+		lastMouse.y += (mouse.y - lastMouse.y)/10;
 	});
-});
+
+
+	$('img.wolfpreview').each(function(){
+		var el = $(this);
+		var offset = el.offset();
+		var width = el.width();
+		var height = el.height();
+		var obj = {
+			el: this,
+			top: offset.top,
+			left: offset.left,
+			width:width,
+			height:height,
+			rotationX:0,
+			rotationY:0,
+			isActive: false
+		}
+		wolves.push(obj);
+	});
+
+
+	$('img.wolfpreview').hover(function(){
+		var i = $('img.wolfpreview').index(this);
+		wolves[i].isActive = true ;
+		AF.start();
+	}, function(){
+		var i = $('img.wolfpreview').index(this);
+		wolves[i].isActive = false ;
+	});
+
+
+	function wolfHover(){
+		for(var i = 0; i< wolves.length; i++){
+			var wolf = wolves[i];
+			//is not active but has rotation values left
+			if((!wolf.isActive) && (wolf.rotationY != 0 || wolf.rotationX != 0)){
+
+				wolf.rotationX += (0-wolf.rotationX)/10;//set to 0 in 10 frames
+				wolf.rotationY += (0-wolf.rotationY)/10;//set to 0 in 10 frames
+
+				//wolf.el.style.transform = 'rotateX('+wolf.rotationX+'deg) rotateY('+wolf.rotationY+'deg)';
+			}
+			// not active
+			else if( !wolf.isActive ) continue 
+			// it is active and should be rotating with mouse
+		else{
+			coordY = (((wolf.top-$(document).scrollTop()-mouse.y)/wolf.height)*2)+1;
+			coordX = ((lastMouse.x-wolf.left)/wolf.width*2)-1;
+			maxDeg = 70;
+
+			//switched X and Y on purpose
+			wolf.rotationY = ((coordX*maxDeg)-wolf.rotationX)/10;
+			wolf.rotationX = ((coordY*maxDeg)-wolf.rotationY)/10;
+
+			console.log('x:'+coordX);
+			wolf.el.style.transform = 'rotateX('+wolf.rotationX+'deg) rotateY('+wolf.rotationY+'deg)';
+		}
+		if(!wolf.isActive){
+			AF.stop();
+		}
+	}
+
+}
+
+AF.add('wolfHover', function(){
+	wolfHover();
+});	
 
 });
+
+
+
+
+
+
+////ANIMATION FRAME ////
+var queue = [];
+var stopList = {};
+var fn =   
+window.requestAnimationFrame        ||
+window.webkitRequestAnimationFrame ||
+window.mozRequestAnimationFrame    ||
+window.oRequestAnimationFrame       ||
+window.msRequestAnimationFrame     ||
+function(/* function */ callback, /* DOMElement */ element){
+	window.setTimeout(callback, 1000 / 10);
+};
+
+var AF =  {
+	stopped:true,
+	animStopped: function (name){
+		if(typeof name == 'undefined') return undefined;
+		if(typeof stopList[name] == 'undefined') return undefined;
+		return stopList[name];
+	},
+	add: function(name, callback){
+		var theAnim = {
+			name:name,
+			callback:callback,
+			stopped:false
+		};
+		stopList[name] = true;
+		queue.push(theAnim);
+	},
+	remove: function(name){
+		for(var i=0; i<queue.length; i++){
+			if(queue[i].name === name){
+				queue.splice(i,1);
+			}
+		}
+		delete stopList[name];
+	},
+	anim: function(){
+		var scope = this;
+		if(!this.stopped){
+			var callback;
+			for(var i=0; i<queue.length; i++){
+				if(!queue[i].stopped){
+					callback = queue[i].callback;
+					callback();
+				}
+			}
+			fn(function(){
+				scope.anim();
+			});
+		}
+	},
+	start: function(name){
+		if(typeof name !== 'undefined'){
+			stopList[name] = false;
+			for(var i=0; i<queue.length; i++){
+				if(queue[i].name === name){
+					queue[i].stopped = false;
+				}
+			}
+		}
+		if(this.stopped){
+			this.stopped = false;
+			this.anim();
+		}
+	},
+	stop: function(name){
+		if(typeof name === 'undefined'){
+			this.stopped = true;
+		}else{
+			var stillAnims = false;
+			stopList[name] = true;
+			for(var i=0; i<queue.length; i++){
+				if(queue[i].name === name){
+					queue[i].stopped = true;
+				}
+				if(!queue[i].stopped) stillAnims = true;
+			}
+			if(!stillAnims) this.stopped = true;
+		}
+	},
+	clearQueue:function(stop){
+		queue = [];
+		if(stop){
+			this.stopped = true;
+		}
+	}
+};
